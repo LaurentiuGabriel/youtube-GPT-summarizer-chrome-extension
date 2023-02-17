bf24827b496df7522df6bc93b3a998db60ac6ae0
Laurentiu Raducu <laurentiu_raducu@iptiq.com>
2023-02-17 13:23:03 +0100

    initial commit

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,12 @@
 // Create a context menu item
 chrome.contextMenus.create({
-  id: "terms",
-  title: "Terms and Conditions Checker",
+  id: "youtube-summary",
+  title: "Summarize Youtube Video",
   contexts: ["all"],
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  chrome.tabs.sendMessage(tab.id, { type: "CHECK", url: tab.url });
+  if (info.menuItemId === "youtube-summary") {
+    chrome.tabs.sendMessage(tab.id, { type: "SUMMARIZE", url: tab.url });
+  }
 });
--- extension/content.js
+++ extension/content.js
@@ -1,20 +1,105 @@
+function extractVideoId(url) {
+  const regex = /watch\?v=(\w{11})/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+}
+
+// Extracts the captions JSON from the HTML of the video page
+function _extractCaptionsJson(html, videoId) {
+  const splittedHtml = html.split('"captions":');
+
+  // Check for too many requests
+  if (splittedHtml.length <= 1) {
+    if (html.includes('class="g-recaptcha"')) {
+      throw new Error(`TooManyRequests: ${videoId}`);
+    }
+    // Check for video availability
+    if (!html.includes('"playabilityStatus":')) {
+      throw new Error(`VideoUnavailable: ${videoId}`);
+    }
+    // Check for transcripts availability
+    throw new Error(`TranscriptsDisabled: ${videoId}`);
+  }
+
+  // Extract the captions JSON
+  const captionsJson = JSON.parse(
+    splittedHtml[1]
+      .split(',"videoDetails')[0]
+      .replace(/\n/g, '')
+  ).playerCaptionsTracklistRenderer;
+
+  // Check for transcripts availability
+  if (!captionsJson) {
+    throw new Error(`TranscriptsDisabled: ${videoId}`);
+  }
+
+  // Check for the presence of caption tracks
+  if (!captionsJson.captionTracks) {
+    throw new Error(`NoTranscriptAvailable: ${videoId}`);
+  }
+
+  return captionsJson;
+}
+
+// Fetches the captions of the video as XML
+async function getCaptionAsXml(youtubeLink) {
+  const youtubeId = extractVideoId(youtubeLink)
+  //const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const targetUrl = 'https://www.youtube.com/watch?v=' + youtubeId;
+  return fetch(targetUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}: ${response.statusText}`);
+      }
+      return response.text();
+    })
+    .then(data => {
+      const link = _extractCaptionsJson(data.toString(), youtubeId)
+      return fetch(link.captionTracks[0].baseUrl)
+        .then(response => response.text())
+        .then(data => {
+          return data
+        })
+        .catch(error => {
+          alert(error)
+          console.log(error);
+        });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function getTextFromXML(xml) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(xml, "application/xml");
+  var textNodes = doc.getElementsByTagName("text");
+  var text = "";
+  for (var i = 0; i < textNodes.length; i++) {
+    text += textNodes[i].textContent;
+  }
+  return text;
+}
+
 // Listen for messages from the background script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "CHECK") {
+  if (message.type === "SUMMARIZE") {
     let text;
 
     // If there's an active text input
-      text = window.getSelection().toString();
-        fetch("http://localhost:3000/eula-summary", {
+    if (message.url.includes("https://www.youtube.com/")) {
+      text = message.url;
+      getCaptionAsXml(text).then(data => {
+        fetch("http://localhost:3000/youtube-summary", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({ message: data }),
+          body: JSON.stringify({ message: getTextFromXML(data) }),
         })
           .then((response) => response.json())
           .then(async (data) => {
-            alert(`Checker: ${data.reply}`);
+            alert(`Summary: ${data.reply}`);
             restoreCursor();
           })
           .catch((error) => {
@@ -24,11 +109,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             );
             throw new Error(error);
           });
-    // } else {
-    //   alert(
-    //     "Something went wrong! Make sure the terms and conditions are not too large to be handled for ChatGPT. Try to send less text!"
-    //   );
-    // }
+      })
+    } else {
+      alert(
+        "This is not a YouTube video! Please go to the page of a youtube video."
+      );
+    }
   }
 });
 
